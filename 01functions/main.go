726d8c310477fde9703ce7dadd226d51802e4b87
javed-iqubal <jiqbal07@gmail.com>
2026-04-05 01:32:18 +0530

    initial commit

---
+++ 01functions/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// function syntax
+
+/*
+	func functionName(args) returnType{
+		// function body
+	}
+*/
+
+func greet() {
+
+	fmt.Println("Hello World")
+}
+
+func main() {
+
+	// invoke function
+
+	greet()
+}
